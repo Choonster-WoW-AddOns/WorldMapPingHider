200e9d85449173abddc56792ec5e968a2842bb41
Choonster TheMage <Choonster.2010@gmail.com>
2016-10-27 21:43:24 +1100

    Update to 7.1 (70100)

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+## 1.03
+- Update to 7.1 (70100)
+
+## 1.02
+- Bump TOC Interface version to 6.0
+
+## 1.01
+- Add README.md for GitHub
+- Add .pkgmeta for Curse packager
+- Change TOC Version field to @package-version@ for Curse packager
+- Add LICENSE.txt
+
+## 1.00
+- AddOn created!
--- core.lua
+++ core.lua
@@ -1,2 +1,4 @@
-﻿local ping = WorldMapPing
-ping:SetScript("OnShow", ping.Hide)
+﻿-- I haven't found any documentation on this method and don't fully understand what the arguments do, so I haven't given them proper names
+hooksecurefunc(WorldMapUnitPositionFrame, "StartPlayerPing", function(self, arg1, arg2)
+	self:StopPlayerPing()
+end)
